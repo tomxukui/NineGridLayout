539b800548df482f72463032fbdf695877abd573
tom <152780382@qq.com>
2018-05-20 12:53:46 +0800

    v1.0.2

--- app/src/main/java/com/ablingbling/app/ninegridlayout/MainActivity.java
+++ app/src/main/java/com/ablingbling/app/ninegridlayout/MainActivity.java
@@ -37,7 +37,7 @@ public class MainActivity extends AppCompatActivity {
             List<String> l = new ArrayList<>();
             list.add(l);
 
-            for (int j = 0; j < 1 + i; j++) {
+            for (int j = 0; j < i; j++) {
                 l.add(images[j]);
             }
         }
